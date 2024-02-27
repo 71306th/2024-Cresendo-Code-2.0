70ef3f9158d9c6848c63ffa459719a907ff0bc87
Ray526 <122028191+Ray526@users.noreply.github.com>
2024-02-27 13:05:35 +0800

    ColorSensor Shit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -19,15 +21,38 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private AddressableLED m_ledleft;
+  private AddressableLED m_ledright;
+  private AddressableLEDBuffer m_ledBuffer;
+
+  public AddressableLEDBuffer getLedBuffer() {
+    return m_ledBuffer;
+  }
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    m_ledleft = new AddressableLED(9);
+    m_ledright = new AddressableLED(10);
+  
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(18);
+    m_ledleft.setLength(m_ledBuffer.getLength());
+    m_ledright.setLength(m_ledBuffer.getLength());
+  
+    // Set the data
+    m_ledleft.setData(m_ledBuffer);
+    m_ledleft.start();
+    m_ledright.setData(m_ledBuffer);
+    m_ledright.start();
   }
 
   /**
